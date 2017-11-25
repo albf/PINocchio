6430770832ffffb8f758e80b1caae754711e76a4
albf <albf.unicamp@gmail.com>
2017-11-25 07:13:57 -0500

    Add stopwatcher
    Provide functions to print wall and cpu time.

---
+++ examples/stopwatch.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+static struct timespec ts_start, ts_end;
+static struct timeval tv_start, tv_end;
+static clock_t c_start;
+
+
+void __attribute__((optimize("O0"))) stopwatch_start() {
+    gettimeofday(&tv_start, NULL);
+    c_start = clock();
+}
+
+void __attribute__((optimize("O0"))) stopwatch_stop() {
+    gettimeofday(&tv_end, NULL);
+
+    double wall = (1000000.0 * (double) (tv_end.tv_sec - tv_start.tv_sec));
+    wall += ((double) (tv_end.tv_usec - tv_start.tv_usec));
+
+    double cpu = 1000000*(double) (clock() - c_start) / CLOCKS_PER_SEC;
+
+    printf("@WALL: %lf\n", wall);
+    printf("@CPU: %lf\n", cpu);
+}
---
+++ examples/stopwatch.h
@@ -0,0 +1,7 @@
+#ifndef STOPWATCH_H_
+#define STOPWATCH_H_
+
+void stopwatch_start();
+void stopwatch_stop();
+
+#endif
